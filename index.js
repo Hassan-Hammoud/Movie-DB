b9d33fcf529c42388e2b219733317489b8467e95
Hassan Hammoud <hassanhammoud2000@gmail.com>
2022-12-30 15:03:36 +0200

    step 3

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const port = 3000;
 app.get("/", (req,res) => {
     res.send("ok");
 });
+app.get("/test", (req,res)=>{
+    res.send({status:200, message:"ok"})
+})
+
+app.get("/time", (req, res) => {
+  res.send({ status:200, message:Date()});
+});
 
 
 app.listen(port, ()=> {
